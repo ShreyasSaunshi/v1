b0b68a10cb5cf5bff4bb76811c9f45b42a70041d
Shreyas Saunshi <67939784+ShreyasSaunshi@users.noreply.github.com>
2025-04-23 16:51:42 +0530

    init

---
+++ lib/data.ts
@@ -0,0 +1,97 @@
+import { IProject } from '@/types';
+
+export const GENERAL_INFO = {
+    email: 'shreyasssaunshi@gmail.com',
+
+    emailSubject: "Let's collaborate on a project",
+    emailBody: 'Hi Shreyas , I am reaching out to you because...',
+};
+
+export const SOCIAL_LINKS = [
+    { name: 'github', url: 'https://github.com/shreyassaunshi' },
+    { name: 'linkedin', url: 'https://www.linkedin.com/in/shreyassaunshi' },
+    { name: 'instagram', url: 'https://www.instagram.com/shreyassaunshi' },
+];
+
+export const MY_STACK = {
+    frontend: [
+        {
+            name: 'Javascript',
+            icon: '/logo/js.png',
+        },
+        {
+            name: 'Typescript',
+            icon: '/logo/ts.png',
+        },
+        {
+            name: 'React',
+            icon: '/logo/react.png',
+        },
+        {
+            name: 'Next.js',
+            icon: '/logo/next.png',
+        },
+        {
+            name: 'Redux',
+            icon: '/logo/redux.png',
+        },
+        {
+            name: 'Tailwind CSS',
+            icon: '/logo/tailwind.png',
+        },
+        {
+            name: 'Frammer Motion',
+            icon: '/logo/framer-motion.png',
+        },
+        {
+            name: 'Bootstrap',
+            icon: '/logo/bootstrap.svg',
+        },
+    ],
+    backend: [
+        {
+            name: 'Node.js',
+            icon: '/logo/node.png',
+        },
+        {
+            name: 'Nest.js',
+            icon: '/logo/nest.svg',
+        },
+        {
+            name: 'Express.js',
+            icon: '/logo/express.png',
+        },
+    ],
+    database: [
+        {
+            name: 'MySQL',
+            icon: '/logo/mysql.svg',
+        },
+        {
+            name: 'PostgreSQL',
+            icon: '/logo/postgreSQL.png',
+        },
+        {
+            name: 'MongoDB',
+            icon: '/logo/mongodb.svg',
+        },
+        {
+            name: 'Prisma',
+            icon: '/logo/prisma.png',
+        },
+    ],
+    tools: [
+        {
+            name: 'Git',
+            icon: '/logo/git.png',
+        },
+        {
+            name: 'Docker',
+            icon: '/logo/docker.svg',
+        },
+        {
+            name: 'AWS',
+            icon: '/logo/aws.png',
+        },
+    ],
+};
